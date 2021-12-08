6f7b99cee07b5139f645da4470afb09e5e649bf1
Ishan koradia <koradiaishan335@gmail.com>
2021-12-08 18:03:54 +0530

    config changes

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect(`mongodb+srv://Ishank:Ishan123@ishanprojects.gvwjq.mongodb.net/expensetracker?
+        retryWrites=true&w=majority`, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
