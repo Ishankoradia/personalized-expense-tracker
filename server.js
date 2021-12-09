aec70d603aa024e2ca90e30c13da247635013608
Ishan koradia <koradiaishan335@gmail.com>
2021-12-09 10:35:30 +0530

    server config changes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 dotenv.config({path: './config/config.env'});
 
@@ -13,12 +14,21 @@ const transactions = require('./routes/transaction');
 const { connect } = require('./routes/transaction');
 
 const app = express();
+// const corsOptions ={
+//     origin:'http://localhost:3000', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+
+app.use(cors());
 
 app.use(express.json());
 
 if(process.env.NODE_ENV == 'development'){
     app.use(morgan('dev'));
 }
+
+
 
 app.use('/api/v1/transactions', transactions);
 
